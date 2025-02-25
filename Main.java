4ba60c96317f2b10c984d8a14523e684c6a57f40
Jazmine Jordan <jazminejordan77@gmail.com>
2025-02-24 23:30:21 -0500

    testing

--- Main.java
+++ Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         Train myTrain = new Train(FuelType.STEAM, 50, 100, 5, 102);
         Car myCar = myTrain.getCar(4);
-        Car myOtherCar = myTrain.getCar(1);
         Random rand = new Random();
         for (int i = 0; i < 50; i++) {
             Passenger p = new Passenger("Louis");
@@ -17,10 +16,9 @@ public class Main {
             p.boardCar(randCar);
         }
         System.out.println(myCar.getCapacity());
-        System.out.println(myOtherCar.getCapacity());
         System.out.println(myCar.seatsRemaining());
-        myCar.printManifest();
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println(myTrain.getMaxCapacity());
+        System.out.println(myTrain.seatsRemaining());
         myTrain.printManifest();
 
     }
